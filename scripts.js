3f6ea4e7e3098364dd9c1795aa2ca8084115fd4c
Horia Ene <93551732+horiaene@users.noreply.github.com>
2021-12-01 23:56:37 +0200

    minor changes
    added UI elements
    comments

--- scripts.js
+++ scripts.js
@@ -1,31 +1,27 @@
-function computerPlay() {   //computer chooses
+function computerPlay() {   //computer's choice
     let variants = ["Rock", "Paper", "Scissors"];
     let computerChoice = variants[Math.floor(Math.random() * variants.length)];
     return computerChoice;
 }
 
 function playRound(playerSelection, computerSelection) { //one round
+    let win = "win";
+    let lose = "lose";
+    let tie = "tie";
     if (playerSelection.toUpperCase() === computerSelection.toUpperCase()) {
-        let result = "Tie!";
-        return result;
+        return tie;
     } else if (playerSelection.toUpperCase() === "ROCK" && computerSelection.toUpperCase() === "SCISSORS" ) {
-        let result = "You win!";
-        return result;
+        return win;
     } else if (playerSelection.toUpperCase() === "ROCK" && computerSelection.toUpperCase() === "PAPER" ) {
-        let result = "You lose!";
-        return result;
+        return lose;
     } else if (playerSelection.toUpperCase() === "PAPER" && computerSelection.toUpperCase() === "SCISSORS" ) {
-        let result = "You lose!";
-        return result;
+        return lose;
     } else if (playerSelection.toUpperCase() === "PAPER" && computerSelection.toUpperCase() === "ROCK" ) {
-        let result = "You win!";
-        return result;
+        return win;
     } else if (playerSelection.toUpperCase() === "SCISSORS" && computerSelection.toUpperCase() === "ROCK" ) {
-        let result = "You lose!";
-        return result;
+        return lose;
     } else if (playerSelection.toUpperCase() === "SCISSORS" && computerSelection.toUpperCase() === "PAPER" ) {
-        let result = "You win!";
-        return result;
+        return win;
 }
 }
 
@@ -33,22 +29,24 @@ function game() { //one game of five rounds
     //set initial score
     let playerScore = 0;
     let computerScore = 0;
+    alert(`Welcome to a game of Rock, Paper, Scissors! Best out of 5 wins.`)
     //repeat function - WIP
         for (let i = 0; i < 5; i++) { //repeat following code 5 times
             let playerSelection = prompt("Rock, paper, scissors? What do you choose?"); //check user choice
             let computerSelection = computerPlay(); //check computer choice
             let roundResult = playRound(playerSelection, computerSelection); //get the result of one round
-                if (roundResult === ("You win!")) { //add one point to user if user wins
+                if (roundResult === "win") { //add one point to user if user wins
                     playerScore = playerScore + 1;
                     alert(`Player wins! Player score: ${playerScore}, computer score: ${computerScore}.`)
-                } else if (roundResult === ("You lose!")) { //add one point to CPU if CPU wins
+                } else if (roundResult === "lose") { //add one point to CPU if CPU wins
                     computerScore = computerScore +  1;
                     alert(`Computer wins! Player score: ${playerScore}, computer score: ${computerScore}.`)
-                } else if (roundResult === ("Tie!")) { //add one point to CPU if CPU wins
+                } else if (roundResult === "tie") { //add one point to CPU if CPU wins
                     alert(`It's a tie! Player score: ${playerScore}, computer score: ${computerScore}.`)
 
     }
 }
+    //check score
     if (playerScore > computerScore) {
         alert(`Player wins!`)
     }  else if (playerScore === computerScore) {
