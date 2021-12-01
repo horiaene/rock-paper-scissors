6ec2c245e1e11c6c1e6eb8253c7e1c760f46bf76
Horia Ene <93551732+horiaene@users.noreply.github.com>
2021-12-01 22:58:11 +0200

    Fixed loop
    Loop wasn't printing score correctly

--- scripts.js
+++ scripts.js
@@ -1,59 +1,56 @@
-let variants = ["Rock", "Paper", "Scissors"];
-function computerPlay() {    
+function computerPlay() {   //computer chooses
+    let variants = ["Rock", "Paper", "Scissors"];
     let computerChoice = variants[Math.floor(Math.random() * variants.length)];
     return computerChoice;
 }
 
-let playerSelection = prompt("What is your selection?");
-let computerSelection = computerPlay();
-
-function playRound(playerSelection, computerSelection) {
-    // let win = "You win!";
-    // let lose = "You lose!";
-    // let tie = "It's a tie!"
-    if (playerSelection === computerSelection) {
+function playRound(playerSelection, computerSelection) { //one round
+    if (playerSelection.toUpperCase() === computerSelection.toUpperCase()) {
         let result = "Tie!";
         return result;
-    } else if (playerSelection === "Rock" && computerSelection === "Scissors" ) {
+    } else if (playerSelection.toUpperCase() === "ROCK" && computerSelection.toUpperCase() === "SCISSORS" ) {
         let result = "You win!";
         return result;
-    } else if (playerSelection === "Rock" && computerSelection === "Paper" ) {
+    } else if (playerSelection.toUpperCase() === "ROCK" && computerSelection.toUpperCase() === "PAPER" ) {
         let result = "You lose!";
         return result;
-    } else if (playerSelection === "Paper" && computerSelection === "Scissors" ) {
+    } else if (playerSelection.toUpperCase() === "PAPER" && computerSelection.toUpperCase() === "SCISSORS" ) {
         let result = "You lose!";
         return result;
-    } else if (playerSelection === "Paper" && computerSelection === "Rock" ) {
+    } else if (playerSelection.toUpperCase() === "PAPER" && computerSelection.toUpperCase() === "ROCK" ) {
         let result = "You win!";
         return result;
-    } else if (playerSelection === "Scissors" && computerSelection === "Rock" ) {
+    } else if (playerSelection.toUpperCase() === "SCISSORS" && computerSelection.toUpperCase() === "ROCK" ) {
         let result = "You lose!";
         return result;
-    } else if (playerSelection === "Scissors" && computerSelection === "Paper" ) {
+    } else if (playerSelection.toUpperCase() === "SCISSORS" && computerSelection.toUpperCase() === "PAPER" ) {
         let result = "You win!";
         return result;
 }
 }
-// let playerSelection = prompt("What is your selection?");
-// let computerSelection = computerPlay();
-// console.log(playRound(playerSelection, computerSelection));
 
-function game() {
+function game() { //one game of five rounds
     //set initial score
     let playerScore = 0;
     let computerScore = 0;
     //repeat function - WIP
-    for (let i = 0; i < 5; i++) {
-    let roundResult = playRound(playerSelection, computerSelection);
-    if (roundResult === ("You win!")) {
-        playerScore = playerScore + 1;
-    } else if (roundResult === ("You lose!")) {
-        computerScore = computerScore + 1;
-    }   
+        for (let i = 0; i < 5; i++) { //repeat following code 5 times
+            let playerSelection = prompt("What is your selection?"); //check user choice
+            let computerSelection = computerPlay(); //check computer choice
+            let roundResult = playRound(playerSelection, computerSelection); //get the result of one round
+                if (roundResult === ("You win!")) { //add one point to user if user wins
+                    playerScore = playerScore + 1;
+                    alert(`Player score: ${playerScore}, computer score: ${computerScore}.`)
+                } else if (roundResult === ("You lose!")) { //add one point to CPU if CPU wins
+                    computerScore = computerScore +  1;
+                    alert(`Player score: ${playerScore}, computer score: ${computerScore}.`)
+                } else if (roundResult === ("Tie!")) { //add one point to CPU if CPU wins
+                    computerScore = computerScore +  1;
+                    alert(`Player score: ${playerScore}, computer score: ${computerScore}.`)
+
+    }
+}
     let score = `Player score: ${playerScore}, computer score: ${computerScore}.`;
     return score;
-}
-
-}
-
+}   
 console.log(game());
